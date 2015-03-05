eb83356a9d9a2fe12b5bf5fdb56b492ab0cc3aba
Nick McKoy <nmckoy10@gmail.com>
2015-03-05 15:30:06 +0000

    more nodeschool

--- nodeschool/learnyounode/program.js
+++ nodeschool/learnyounode/program.js
@@ -1,5 +1,6 @@
 //console.log(process.argv);
 var fs = require('fs');
+var path = require("path");
 var sum = 0;
 
 // for (var i in process.argv) {
@@ -9,18 +10,44 @@ var sum = 0;
 // }
     
 //var fileStr = fs.readFileSync(process.argv[2]).toString();
-var fileStr;
-function read(){
-    fs.readFile(process.argv[2], function done(err, data) {
+// var fileStr;
+// function read(callback){
+//     fs.readFile(process.argv[2], function a(err, data) {
+//         if (err){
+//             console.log("couldnt read file " + err);
+//             return;    
+//         } else {
+//             fileStr = String(data);
+//         }
+//         callback();
+//     });
+// }
+
+// function print(){
+//     var nlarr = fileStr.split("\n");
+//     console.log(nlarr.length - 1);
+// }
+// console.log("argv[2] = " + process.argv[2] + 
+//             "\nargv[3] = " + process.argv[3]);
+var fileList;
+function readir(callback) {
+    fs.readdir(process.argv[2], function a(err, data){
         if (err){
-            console.log("couldnt read file " + err);
-            return;    
+            console.log("couldnt get path " + err);
+            return;
         } else {
-            fileStr = String(data);
+            fileList = String(data);
         }
+        callback(fileList);
     });
 }
-read();
-var nlarr = fileStr.split("\n");
-
-console.log(nlarr.length - 1);
+function filter(list) {
+    // console.log("callback called");
+    var filter = list.filter(function(fn){
+        return path.extname(fn) === "."+String(process.argv[3]);
+    });
+    console.log(filter);
+    
+}
+readir(filter);
+//read(print);
